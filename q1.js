7cc9f081c4c3352731818db29282bbbee3322fe0
30nishant <32567360+30nishant@users.noreply.github.com>
2019-01-14 16:25:13 +0530

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,14 @@
+//person object with various properties
+let person = {
+    firstName: 'Nishant',
+    lastname: 'Arora',
+    age: 21,
+    hometown: 'Delhi',
+    college: 'Manipal University Jaipur',
+    company: 'Celigo'
+};
+console.log('Name: ' + person.firstName + ' ' + person.lastname);
+console.log('Age: ' + person.age);
+console.log('From ' + person.hometown);
+console.log('Studied at ' + person.college);
+console.log('Works at ' + person.company);
